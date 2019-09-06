4acb3a173621cc0ba524b7125473b472136b0e98
michellejanosi <michelle.janosi@gmail.com>
2019-09-06 09:42:52 -0400

    Update tweet length from 140 to 280

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,4 @@
 class Quote < ApplicationRecord
-  validates :quote, presence: true, length: { maximum: 140, minimum: 5 }
+  validates :quote, presence: true, length: { maximum: 280, minimum: 5 }
   validates :author, presence: true, length: { maximum: 50, minimum: 3 }
 end
